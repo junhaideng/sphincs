99c56cb39755d7808fef10553f00aaccb98e5e23
junhaideng <201648748@qq.com>
2022-05-19 22:47:19 +0800

    feat: test sha

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -2,18 +2,30 @@ package hash
 
 import "testing"
 
+type HashArgs struct {
+	msg [][]byte
+}
+
 func BenchmarkHash(b *testing.B) {
-	msg := make([]byte, 256 * 66)
+	block := []int{34, 67, 133, 265, 66, 131, 261, 522}
+	data := make([][]byte, len(block)*2)
+	for i, b := range block {
+		data[i*2] = make([]byte, b*256/8)
+		data[i*2+1] = make([]byte, b*512/8)
+	}
 
-	b.Run("sha256", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			_ = Sha256(msg)
-		}
-	})
+	for j, v := range block {
+		b.Logf("block num: %d\n", v)
+		b.Run("sha256", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Sha256(data[j*2])
+			}
+		})
 
-	b.Run("sha512", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			_ = Sha512(msg)
-		}
-	})
+		b.Run("sha512", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Sha512(data[j*2+1])
+			}
+		})
+	}
 }
